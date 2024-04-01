479e24ca627a6c00ca22fd3f7e2bf987592e967f
fahmed0710 <fahmed0710@gmail.com>
2024-04-01 01:19:52 -0700

    Created basic Graph menu page from where users can access different graph puzzles

---
+++ dev/frontend/app/(home)/graph/page.tsx
@@ -0,0 +1,12 @@
+export default function Graph() {
+  return (
+    <div>
+      <h1 className="text-center font-bold">Graphs</h1>
+      <p className="text-center">Click on a link to access a graph puzzle!</p>
+      <a 
+        href="/graph/1"
+        className="font-medium text-blue-500 hover:undlerline"
+      >1</a>
+    </div>
+  )
+}
